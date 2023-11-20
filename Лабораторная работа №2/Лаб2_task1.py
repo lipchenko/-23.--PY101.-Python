fddce5fead660484e58f493dadecc05a8fca3252
lipchenko <150557866+lipchenko@users.noreply.github.com>
2023-11-20 23:11:07 +0300

    Add files via upload

---
+++ Лабораторная работа №2/Лаб2_task1.py
@@ -0,0 +1,19 @@
+money_capital = 20000  # Подушка безопасности
+salary = 5000  # Ежемесячная зарплата
+spend = 6000  # Траты за первый месяц
+increase = 0.05  # Ежемесячный рост цен
+
+months = 0  # Счетчик месяцев без долгов
+
+while money_capital >= 0:
+    budget = salary + money_capital  # Бюджет текущего месяца
+    if spend > budget:
+     break  # Прерываем цикл, если траты превышают бюджет
+    money_capital -= spend - salary
+    spend *= (1 + increase)
+    months += 1
+
+
+# TODO Посчитайте количество  месяцев, которое можно протянуть без долгов
+
+print("Количество месяцев, которое можно протянуть без долгов:", months)
---
+++ Лабораторная работа №2/Лаб2_task2.py
@@ -0,0 +1,15 @@
+salary = 5000  # Ежемесячная зарплата
+spend = 6000  # Траты за первый месяц
+months = 10  # Количество месяцев, которое планируется протянуть без долгов
+increase = 0.03  # Ежемесячный рост цен
+
+money_capital = (spend - salary) / (1 - increase)
+for i in range  (1,months):
+    spend *= (1 + increase)
+    money_capital += round(money_capital, 2)
+
+
+
+# TODO Рассчитайте подушку безопасности, чтобы протянуть 10 месяцев без долгов
+
+print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", money_capital)
