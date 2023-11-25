c093814ca939e049fdf127332399129327d60e9d
lipchenko <150557866+lipchenko@users.noreply.github.com>
2023-11-25 22:11:29 +0300

    Update Лаб2_task2.py

--- Лабораторная работа №2/Лаб2_task2.py
+++ Лабораторная работа №2/Лаб2_task2.py
@@ -3,13 +3,12 @@ spend = 6000  # Траты за первый месяц
 months = 10  # Количество месяцев, которое планируется протянуть без долгов
 increase = 0.03  # Ежемесячный рост цен
 
-money_capital = (spend - salary) / (1 - increase)
-for i in range  (1,months):
-    spend *= (1 + increase)
-    money_capital += round(money_capital, 2)
+money_capital = 0
 
+for i in range(1, months):
+    spend *= (1 + increase)  # Увеличение расходов на 3% в каждый следующий месяц
 
-
+money_capital += spend - salary
 # TODO Рассчитайте подушку безопасности, чтобы протянуть 10 месяцев без долгов
 
-print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", money_capital)
+print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", round(money_capital, 2))
